20ee06e62ee550e48b57cfcde28aa3830f0de171
Sean Cribbs <seancribbs@gmail.com>
2017-09-24 21:39:32 -0500

    Address licensing issues for clarity.

--- src/binomial_heap.erl
+++ src/binomial_heap.erl
@@ -1,4 +1,18 @@
-%% Based on Okasaki 6.2.2
+%% Copyright 2014 Sean Cribbs
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+%% @doc Binomial heap based on Okasaki 6.2.2
 -module(binomial_heap).
 -export([new/0, insert/2, insert/3, merge/2, delete/1, to_list/1, take/2, size/1]).
 -record(node,{
